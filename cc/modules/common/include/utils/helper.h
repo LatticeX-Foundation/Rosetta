6b4a9e935fc636f1cb7eb198c722f33b989c40bd
ujnss <unlinunlin@163.com>
2020-07-23 15:39:36 +0800

    Add log_2, for integers, supports log2ceil and log2floor

--- cc/modules/common/include/utils/helper.h
+++ cc/modules/common/include/utils/helper.h
@@ -327,3 +327,26 @@ static inline double to_double(const char* p) {
   }
   return r;
 }
+
+/**
+ * log_2(integer)
+ */
+static const int tab64[64] = {63, 0,  58, 1,  59, 47, 53, 2,  60, 39, 48, 27, 54, 33, 42, 3,
+                              61, 51, 37, 40, 49, 18, 28, 20, 55, 30, 34, 11, 43, 14, 22, 4,
+                              62, 57, 46, 52, 38, 26, 32, 41, 50, 36, 17, 19, 29, 10, 13, 21,
+                              56, 45, 25, 31, 35, 16, 9,  12, 44, 24, 15, 8,  23, 7,  6,  5};
+
+static inline uint64_t log2floor(uint64_t value) {
+  value |= value >> 1;
+  value |= value >> 2;
+  value |= value >> 4;
+  value |= value >> 8;
+  value |= value >> 16;
+  value |= value >> 32;
+  return tab64[((uint64_t)((value - (value >> 1)) * 0x07EDD5E59A4E28C2)) >> 58];
+}
+
+static inline uint64_t log2ceil(uint64_t value) {
+  auto floor = log2floor(value);
+  return floor + (value > (1ull << floor));
+}
