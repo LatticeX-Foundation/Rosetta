b9e78b3d7448312b07a93e576c812ea5049c103d
Junjie Shi <shijunjiechn@gmail.com>
2020-09-25 13:20:13 +0800

    Add a naive protocol demo to show how to integrate new customzied backend protocols.

---
+++ cc/modules/protocol/mpc/naive/include/naive_ops_impl.h
@@ -0,0 +1,59 @@
+// ==============================================================================
+// Copyright 2020 The LatticeX Foundation
+// This file is part of the Rosetta library.
+//
+// The Rosetta library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Rosetta library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the Rosetta library. If not, see <http://www.gnu.org/licenses/>.
+// ==============================================================================
+#pragma once
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+#include "cc/modules/protocol/public/include/protocol_base.h"
+#include "cc/modules/protocol/public/include/protocol_ops.h"
+
+namespace rosetta {
+class NaiveOpsImpl : public ProtocolOps {
+public:
+  NaiveOpsImpl(const string& msg_id) : ProtocolOps(msg_id) {}
+  ~NaiveOpsImpl() = default;
+
+  virtual int PrivateInput(int party_id,
+                            const vector<double>& in_x,
+                            vector<string>& out_x);
+
+  int Add(const vector<string>& a,
+          const vector<string>& b,
+          vector<string>& output,
+          const attr_type* attr_info = nullptr);
+  
+  int Sub(const vector<string>& a,
+          const vector<string>& b,
+          vector<string>& output,
+          const attr_type* attr_info = nullptr);
+  
+  int Mul(const vector<string>& a,
+          const vector<string>& b,
+          vector<string>& output,
+          const attr_type* attr_info = nullptr);
+    
+  int Reveal(const vector<string>& a,
+              vector<string>& output,
+              const attr_type* attr_info = nullptr);
+
+public:
+  shared_ptr<NET_IO> io = nullptr;
+};
+
+} // namespace rosetta
