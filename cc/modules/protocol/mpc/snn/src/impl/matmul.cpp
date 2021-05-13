cbc973b60abf852b97e04758aba6ddd13bc00606
YZY010203 <380025985@qq.com>
2021-05-13 09:38:46 +0800

    复原matmul

--- cc/modules/protocol/mpc/snn/src/impl/matmul.cpp
+++ cc/modules/protocol/mpc/snn/src/impl/matmul.cpp
@@ -69,15 +69,9 @@ int MatMul::funcMatMulMPC(
       addVectors<mpc_t>(A1, A2, A, size_left);//算A
       addVectors<mpc_t>(B1, B2, B, size_right);//算B
 
-<<<<<<< HEAD
-      matrixMultEigen(A, B, C, rows, common_dim, columns, transpose_a, transpose_b);//转置
-      subtractVectors<mpc_t>(C, C1, C2, size);//算c2
-      sendVector<mpc_t>(C2, PARTY_B, size);//把C2发给B
-=======
       EigenMatMul(A, B, C, rows, common_dim, columns, transpose_a, transpose_b);
       subtractVectors<mpc_t>(C, C1, C2, size);
       sendVector<mpc_t>(C2, PARTY_B, size);
->>>>>>> upstream/master
     }
 
     if (PRIMARY) {
@@ -135,100 +129,7 @@ int MatMul::funcMatMulMPC(
   }
   return 0;
 }
-<<<<<<< HEAD
 
-//------------------------------------------------------------------------
-int Rsqrt::funcRsqrt(
-  const vector<mpc_t>& a, const vector<mpc_t>& b, vector<mpc_t>& c, size_t rows, size_t common_dim,
-  size_t columns, size_t transpose_a, size_t transpose_b) {
-  
-  if (THREE_PC) {
-    //cout << "-----  " << __FUNCTION__ << " 3PC -----" << endl;
-    size_t size = rows * columns;
-    size_t size_left = rows * common_dim;
-    size_t size_right = common_dim * columns;
-    vector<mpc_t> A(size_left, 0), B(size_right, 0), C(size, 0);
-
-    if (HELPER) {
-      vector<mpc_t> A1(size_left, 0), A2(size_left, 0), B1(size_right, 0), B2(size_right, 0),
-        C1(size, 0), C2(size, 0);
-
-      populateRandomVector<mpc_t>(A1, size_left, "a_1", "POSITIVE");
-      populateRandomVector<mpc_t>(A2, size_left, "a_2", "POSITIVE");
-      populateRandomVector<mpc_t>(B1, size_right, "b_1", "POSITIVE");
-      populateRandomVector<mpc_t>(B2, size_right, "b_2", "POSITIVE");
-      populateRandomVector<mpc_t>(C1, size, "c_1", "POSITIVE");
-
-      addVectors<mpc_t>(A1, A2, A, size_left);//算A
-      addVectors<mpc_t>(B1, B2, B, size_right);//算B
-
-      matrixMultEigen(A, B, C, rows, common_dim, columns, transpose_a, transpose_b);//转置
-      subtractVectors<mpc_t>(C, C1, C2, size);//算c2
-      sendVector<mpc_t>(C2, PARTY_B, size);//把C2发给B
-    }
-
-    if (PRIMARY) {
-      vector<mpc_t> E(size_left), F(size_right);
-      vector<mpc_t> temp_E(size_left), temp_F(size_right);
-      vector<mpc_t> temp_c(size);
-
-      if (partyNum == PARTY_A) {
-        populateRandomVector<mpc_t>(A, size_left, "a_1", "POSITIVE");
-        populateRandomVector<mpc_t>(B, size_right, "b_1", "POSITIVE");
-        populateRandomVector<mpc_t>(C, size, "c_1", "POSITIVE");
-      }
-
-      if (partyNum == PARTY_B) {
-        populateRandomVector<mpc_t>(A, size_left, "a_2", "POSITIVE");
-        populateRandomVector<mpc_t>(B, size_right, "b_2", "POSITIVE");
-        receiveVector<mpc_t>(C, PARTY_C, size);
-      }
-
-      // receiveThreeVectors<mpc_t>(A, B, C, PARTY_C, size_left, size_right, size);
-      subtractVectors<mpc_t>(a, A, E, size_left);
-      subtractVectors<mpc_t>(b, B, F, size_right);
-
-      thread* threads = new thread[2];
-
-      threads[0] = thread(
-        &OpBase_::sendTwoVectors<mpc_t>, this, ref(E), ref(F), adversary(partyNum), size_left,
-        size_right);
-      threads[1] = thread(
-        &OpBase_::receiveTwoVectors<mpc_t>, this, ref(temp_E), ref(temp_F), adversary(partyNum),
-        size_left, size_right);
-
-      for (int i = 0; i < 2; i++)
-        threads[i].join();
-
-      delete[] threads;
-
-      addVectors<mpc_t>(E, temp_E, E, size_left);
-      addVectors<mpc_t>(F, temp_F, F, size_right);
-
-      matrixMultEigen(a, F, c, rows, common_dim, columns, transpose_a, transpose_b);
-      matrixMultEigen(E, b, temp_c, rows, common_dim, columns, transpose_a, transpose_b);
-
-      addVectors<mpc_t>(c, temp_c, c, size);
-      addVectors<mpc_t>(c, C, c, size);
-
-      if (partyNum == PARTY_A) {
-        matrixMultEigen(E, F, temp_c, rows, common_dim, columns, transpose_a, transpose_b);
-        subtractVectors<mpc_t>(c, temp_c, c, size);
-      }
-
-      funcTruncate2PC(c, FLOAT_PRECISION_M, size, PARTY_A, PARTY_B);
-    }
-    //cout << "-----  " << __FUNCTION__ << " 3PC ok. -----" << endl;
-  }
-  return 0;
-}
-
-
-
-
-//-------------------------------------------------------------------------------
 } // namespace mpc
-=======
 } // namespace snn
->>>>>>> upstream/master
 } // namespace rosetta
