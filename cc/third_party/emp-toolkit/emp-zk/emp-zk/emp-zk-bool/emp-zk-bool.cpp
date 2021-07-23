34ba220acb80620e174ece96828583c561df4296
George Shi <shijunjiechn@gmail.com>
2021-07-23 15:03:29 +0800

    remove hard codes of submodules

--- cc/third_party/emp-toolkit/emp-zk/emp-zk/emp-zk-bool/emp-zk-bool.cpp
+++
@@ -1,7 +0,0 @@
-#include "emp-zk/emp-zk-bool/cheat_record.h"
-
-vector<string> CheatRecord::message;
-void CheatRecord::put(const string &s) {
-	message.push_back(s);
-}
-
--- cc/third_party/emp-toolkit/emp-zk/emp-zk/emp-zk.h
+++
@@ -1,9 +0,0 @@
-#ifndef EMP_ZK
-#define EMP_ZK
-
-#include "emp-zk/emp-zk-bool/emp-zk-bool.h"
-#include "emp-zk/emp-zk-arith/emp-zk-arith.h"
-#include "emp-zk/extensions/lowmc.h"
-#include "emp-zk/extensions/floats.h"
-
-#endif
